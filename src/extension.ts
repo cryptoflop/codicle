0887b684635fc66eee0f706e9d787b88b4eb4ca3
Cryptoflop <max.david1007@gmail.com>
2024-02-10 00:58:02 +0100

    rename

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,23 @@ import * as vscode from 'vscode'
 import { createHash } from 'crypto'
 import { gzip } from 'node:zlib'
 
-import { captureScreen, captureWindow, numScreens, focusedWindowId } from "../rs/index"
+import { captureScreen, captureWindow, numScreens, focusedWindowId } from "../screen-capture/index"
 import WebViewProvider from './WebViewProvider'
 
 export function activate(context: vscode.ExtensionContext) {
 	const webViewProvider = new WebViewProvider(context.extensionUri)
 
-	context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider('codicle.mini', webViewProvider)
-  )
+	["codicle-exp", "codicle-scm", "codicle-pnl", "codicle-tst", "codicle-dbg"].forEach(id => context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(id, webViewProvider)
+  ))
+
+	context.subscriptions.push(vscode.commands.registerCommand('codicle.toggle', () => {
+		
+	}))
+
+	const toggleSbi = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1000)
+	toggleSbi.command = "codicle.toggle"
+	context.subscriptions.push(toggleSbi)
 
 	function useCaptureHandler(captureFn: () => { data: Uint8Array } | null, interval = 1000) {
 		let lastHash = ""
@@ -25,7 +33,7 @@ export function activate(context: vscode.ExtensionContext) {
 					webViewProvider.send({ ev: "capture", data: capture.data })
 
 					gzip(capture.data, (err, buffer) => {
-						console.log(buffer.length, capture.data.length);
+						console.log(buffer.length, capture.data.length)
 					})
 				}
 			}
@@ -33,16 +41,18 @@ export function activate(context: vscode.ExtensionContext) {
 		return () => clearInterval(handle)
 	}
 
-	context.subscriptions.push(vscode.commands.registerCommand('codicle.window', () => {
-		const windowId = focusedWindowId()
-		if (windowId === null) { return }
-		useCaptureHandler(() => captureWindow(windowId))
-	}))
+	
 
-	context.subscriptions.push(vscode.commands.registerCommand('codicle.screen', () => {
-		const screenIdx = 0
-		useCaptureHandler(() => captureScreen(screenIdx))
-	}))
+	// context.subscriptions.push(vscode.commands.registerCommand('codicle.window', () => {
+	// 	const windowId = focusedWindowId()
+	// 	if (windowId === null) { return }
+	// 	useCaptureHandler(() => captureWindow(windowId))
+	// }))
+
+	// context.subscriptions.push(vscode.commands.registerCommand('codicle.screen', () => {
+	// 	const screenIdx = 0
+	// 	useCaptureHandler(() => captureScreen(screenIdx))
+	// }))
 
 	// console.log(vscode.env.appName, vscode.env.sessionId)
 }
